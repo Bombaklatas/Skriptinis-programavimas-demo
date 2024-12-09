2de9703015c9ee8531679b187e0074c5db658975
Bombaklatas <seskine.yaho@gmail.com>
2024-12-09 11:34:32 +0200

    atsiskaitymas
    atsyskaityta

--- Togglemode.js
+++ Togglemode.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const modeToggle = document.getElementById('mode-toggle');
+
+    // Check for saved user preference, if any, on load of the website
+    if (localStorage.getItem('dark-mode') === 'true') {
+        document.body.classList.add('dark-mode');
+        modeToggle.textContent = 'Light Mode';
+    }
+
     modeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
-        if (document.body.classList.contains('dark-mode')) {
-            modeToggle.textContent = 'Light Mode';
-        } else {
-            modeToggle.textContent = 'Night Mode';
-        }
+        const isDarkMode = document.body.classList.contains('dark-mode');
+        localStorage.setItem('dark-mode', isDarkMode);
+        modeToggle.textContent = isDarkMode ? 'Light Mode' : 'Night Mode';
     });
 });
